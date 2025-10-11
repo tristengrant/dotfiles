b70a7b91608a833d09e572afb7b0d07ab5b0aab5
Tristen Grant <hello@tristengrant.com>
2025-10-11 04:24:04 -0500

    made helium my default browser

--- nvim/lua/plugins/onedarkpro-theme.lua
+++
@@ -1,8 +0,0 @@
--- ~/.config/nvim/lua/plugins/onedarkpro-theme.lua
-
-return {
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-  },
-}
