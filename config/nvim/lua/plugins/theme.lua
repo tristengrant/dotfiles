8c5f340e517b44eb8dcf0f63b03b690746cf9837
Tristen Grant <hello@tristengrant.com>
2025-11-29 18:21:52 -0600

    Removed Thunar daemon from xinitrc since I've switched to pcmanfm

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,27 +1,28 @@
--- Colour Theme: kanagawa.nvim - GitHub https://github.com/rebelot/kanagawa.nvim
+-- Colour Theme: kanagawa.nvim - https://github.com/rebelot/kanagawa.nvim
+-- Colour Theme: miikanissi/modus-themes.nvim - https://github.com/miikanissi/modus-themes.nvim
+-- Colour Theme: rose-pine/neovim - https://github.com/rose-pine/neovim
+-- Colour Theme: xeind/nightingale.nvim - https://github.com/xeind/nightingale.nvim
+-- Colour Theme: Skardyy/makurai-nvim - https://github.com/Skardyy/makurai-nvim
+-- Current Colour Theme: vague-theme/vague.nvim - https://github.com/vague-theme/vague.nvim
+-- Colour Theme: mistweaverco/retro-theme.nvim - https://github.com/mistweaverco/retro-theme.nvim
 
 return {
-  "rebelot/kanagawa.nvim",
+  -- "rebelot/kanagawa.nvim",
+  -- "miikanissi/modus-themes.nvim",
+  -- "rose-pine/neovim",
+  -- "folke/tokyonight.nvim",
+  -- "xeind/nightingale.nvim",
+  -- "Skardyy/makurai-nvim",
+  "vague-theme/vague.nvim",
+  -- "mistweaverco/retro-theme.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require('kanagawa').setup({
-      compile = false,              -- optional, speeds up loading if true
-      transparent = true,           -- <<== make background transparent
-      terminalColors = true,        -- keep terminal colors in sync
-
-      colors = {
-        theme = {
-          all = {
-            ui = {
-                  bg_gutter = "none"
-                }
-              }
-            }
-          }
+    require('vague').setup({
+      style = "auto",
+      transparent = false,
     })
 
-    vim.cmd("colorscheme kanagawa-dragon")
+    vim.cmd("colorscheme vague")
   end,
 }
-
