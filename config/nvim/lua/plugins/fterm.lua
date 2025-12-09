31d1d8c8ed9977f0020ba30da4ec9811146faf2a
Tristen Grant <hello@tristengrant.com>
2025-12-09 09:17:36 -0600

    Added NeoVim config

---
+++ config/nvim/lua/plugins/fterm.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "numToStr/FTerm.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}
---
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+return {
+  { 
+    'MeanderingProgrammer/render-markdown.nvim',
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+  },
+}
