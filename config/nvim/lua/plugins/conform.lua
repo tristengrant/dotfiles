59fcd87628ffa2b501a3eaafddd7d145ec30d84e
Tristen Grant <hello@tristengrant.com>
2025-12-24 16:19:46 -0600

    Enabled formatting but excluded c code and suckless directores

---
+++ config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,40 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      go = { "gofumpt" },
+      yaml = { "yamlfmt" },
+			c = {},
+      -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      -- typescript = { "prettierd", "prettier", stop_after_first = true },
+    },
+
+    format_on_save = function(bufnr)
+      local fname = vim.api.nvim_buf_get_name(bufnr)
+
+      -- Skip formatting for Suckless source trees
+      local skip_dirs = {
+        "/home/tristen/Projects/suckless/",
+				"dwm",
+        "/dmenu/",
+        "/tabbed/",
+        "/slock/",
+        "/dwmblocks-async/",
+        "/suckless/",
+				"st/",
+      }
+
+      for _, dir in ipairs(skip_dirs) do
+        if fname:find(dir) then
+          return
+        end
+      end
+
+      return {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      }
+    end,
+  },
+}
